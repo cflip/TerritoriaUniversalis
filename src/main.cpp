bd69e64668d499eda74037197238772fc45b5842
cflip <cflip@cflip.net>
2022-04-18 13:52:55 -0600

    Use SFML::View instead of manually scaling the sprite

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "Territoria Universalis");
+	sf::View view = window.getDefaultView();
 
 	sf::Texture map_texture;
 	if (!map_texture.loadFromFile("res/map.png"))
@@ -16,9 +17,7 @@ int main()
 	Map map(map_image);
 	map.AddCountry();
 
-	float zoom = 2.f;
 	int drag_x = 0, drag_y = 0;
-	int pan_x = 0, pan_y = 0;
 	bool is_dragging = false;
 
 	sf::Event event;
@@ -38,16 +37,24 @@ int main()
 				break;
 			case sf::Event::MouseMoved:
 				if (is_dragging) {
-					pan_x += event.mouseMove.x - drag_x;
-					pan_y += event.mouseMove.y - drag_y;
+					// TODO: This should take into account the current zoom level
+					float delta_x = event.mouseMove.x - drag_x;
+					float delta_y = event.mouseMove.y - drag_y;
 					drag_x = event.mouseMove.x;
 					drag_y = event.mouseMove.y;
+
+					view.move(-delta_x, -delta_y);
+					window.setView(view);
 				}
 				break;
-			case sf::Event::MouseWheelScrolled:
-				zoom += event.mouseWheelScroll.delta / 4.f;
-				if (zoom < 0.25f) zoom = 0.25f;
+			case sf::Event::MouseWheelScrolled: {
+				// TODO: This should zoom relative to the mouse pointer's location,
+				//       rather than the center of the screen.
+				float delta = event.mouseWheelScroll.delta + 1.5f;
+				view.zoom(1.f / delta);
+				window.setView(view);
 				break;
+			}
 			case sf::Event::Closed:
 				window.close();
 				break;
@@ -56,8 +63,6 @@ int main()
 
 		map.Draw(map_image);
 		map_texture.loadFromImage(map_image);
-		sprite.setPosition((float)pan_x, (float)pan_y);
-		sprite.setScale(zoom, zoom);
 
 		window.clear({ 0, 8, 4 });
 		window.draw(sprite);
